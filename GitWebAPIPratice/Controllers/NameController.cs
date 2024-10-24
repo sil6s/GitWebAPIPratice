f62cb9e59c711c4ae4d5b3360f6a6211c178f32c
ccsil <ccsil@CURRYSC-LG>
2024-10-24 12:56:14 -0400

    Add project files.

---
+++ GitWebAPIPratice/Controllers/NameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitWebAPIPratice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Silas Curry" });
+        }
+    }
+}
